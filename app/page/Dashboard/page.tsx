6aea96483e168f46ba525b97aefa4703c4bd60b6
rapterxcode <phonepasit9@gmail.com>
2024-09-06 18:46:21 +0700

    main commit

--- app/page/Dashboard/page.tsx
+++ app/page/Dashboard/page.tsx
@@ -2,10 +2,6 @@ import NavOne from "@/components/NavOne"
 import NavTwo from "@/components/NavTwo"
 import PageOriginal from "@/components/pageOriginal"
 
-
-
-
-
 export default function Dashboard() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
--- app/page/Invoice/page.tsx
+++ app/page/Invoice/page.tsx
@@ -2,7 +2,6 @@ import InvoiceBuilder from "@/components/Invoice/InvoiceBuilder";
 import NavOne from "@/components/NavOne";
 import NavTwo from "@/components/NavTwo";
 
-
 export default function InvoicePage() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
