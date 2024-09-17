f28c0fdfed0c4cc65373943b3ac45d64a7644ef5
rapterxcode <phonepasit9@gmail.com>
2024-09-17 16:42:58 +0700

    commit

--- app/page/Dashboard/page.tsx
+++ app/page/Dashboard/page.tsx
@@ -1,18 +1,18 @@
-import NavOne from "@/components/NavOne"
-import NavTwo from "@/components/NavTwo"
-import PageOriginal from "@/components/pageOriginal"
+import NavOne from "@/components/NavOne";
+import NavTwo from "@/components/NavTwo";
+import PageOriginal from "@/components/pageOriginal";
 
-export default function Dashboard() {
+
+export default function InvoicePage() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <NavOne/>
-      <div className="flex flex-col">
-        <NavTwo/>
-        {/* Section Contect */}
-        <PageOriginal/>
-        {/* Section Contect */}
-      </div>
+    <NavOne/>
+    <div className="flex flex-col">
+      <NavTwo/>
+      {/* Section Contect */}
+      <PageOriginal/>
+      {/* Section Contect */}
     </div>
-  )
-};
-
+  </div>
+  );
+}
--- app/page/Invoice/page.tsx
+++ app/page/Invoice/page.tsx
@@ -2,9 +2,7 @@ import InvoiceBuilder from "@/components/Invoice/InvoiceBuilder";
 import NavOne from "@/components/NavOne";
 import NavTwo from "@/components/NavTwo";
 
-
 export default function InvoicePage() {
-  
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
     <NavOne/>
