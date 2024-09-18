732529bd5c053e81d8752f5e8d5317b625b035bc
rapterxcode <phonepasit9@gmail.com>
2024-09-18 14:45:34 +0700

    main commit

--- components/Invoice/Header.tsx
+++ components/Invoice/Header.tsx
@@ -17,15 +17,15 @@ const Header: React.FC<HeaderProps> = ({ logo, setLogo, isPrinting }) => {
       <div className="flex items-center">
         <div className="flex-1">
           <div className="mt-auto p-10 justify-items-start">
-            {logo && <img src={logo} alt="Company Logo" className="w-[200px] h-[auto]" />}
+            {logo && <img src={logo} alt="Company Logo" className="w-[150px] h-[auto]" />}
           </div>
         </div>
       </div>
       <div className="flex flex-col items-end">
         {isPrinting ? (
           <>
-            <div className="text-2xl font-bold text-right mb-2">{companyText}</div>
-            <div className="text-2xl font-bold text-right">{invoiceText}</div>
+            <div className="text-xl font-bold text-right mb-2">{companyText}</div>
+            <div className="text-3xl font-bold text-right">{invoiceText}</div>
           </>
         ) : (
           <>
@@ -33,13 +33,13 @@ const Header: React.FC<HeaderProps> = ({ logo, setLogo, isPrinting }) => {
               type="text" 
               value={companyText} 
               onChange={(e) => setCompanyText(e.target.value)} 
-              className="text-2xl font-bold text-right mb-2 bg-transparent" 
+              className="text-xl font-bold text-right mb-2 bg-transparent" 
             />
             <Input 
               type="text" 
               value={invoiceText} 
               onChange={(e) => setInvoiceText(e.target.value)} 
-              className="text-2xl font-bold text-right bg-transparent" 
+              className="text-3xl font-bold text-right bg-transparent" 
             />
           </>
         )}
