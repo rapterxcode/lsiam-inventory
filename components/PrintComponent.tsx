5659cfe5625aa02df5b4ef02aab7877693d4cd00
rapterxcode <phonepasit9@gmail.com>
2024-09-18 12:56:11 +0700

    main commit

--- components/PrintComponent.tsx
+++ components/PrintComponent.tsx
@@ -1,7 +1,14 @@
 import React, { forwardRef } from 'react';
-
+import "../app/globals.css";
 const PrintComponent = forwardRef<HTMLDivElement, { children: React.ReactNode }>((props, ref) => (
-  <div ref={ref} className="invoice-builder p-6 rounded-lg shadow-md bg-slate-300 max-h-screen overflow-y-auto" style={{ width: '210mm', height: '297mm' }}>
+  <div
+    ref={ref}
+    className="invoice-builder p-6 rounded-lg shadow-md bg-slate-300 max-h-screen overflow-y-auto"
+    style={{
+      width: '210mm',
+      height: '297mm'
+    }}
+  >
     {props.children}
   </div>
 ));
